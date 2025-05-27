64ccda267e52a12c3e6ede4d242d30d7f342c32a
Navin Maharjan <maharjan.nabin85@gmail.com>
2025-05-27 15:14:08 +0545

    feat(cart): add Add to Cart button on product card component

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,16 +1,24 @@
 import Image from "next/image";
+import { Button } from "./ui/button";
 
 const ProductCard = ({ item }) => {
   return (
-    <div className=" flex flex-col">
-      <div className="relative w-full h-[450px] overflow-hidden rounded-lg">
+    <div className=" flex flex-col group cursor-pointer relative">
+      <div className="relative w-full h-[450px] overflow-hidden rounded-lg ">
         <Image
           width={2000}
           height={2000}
           src={item.image}
           alt={item.name}
-          className="absolute w-full h-full object-cover"
+          className="absolute w-full h-full object-cover group-hover:scale-105 transition-all duration-300"
         />
+        <div className="absolute w-full -bottom-16 group-hover:bottom-4 left-0 p-4 transition-all duration-500 ease-in-out">
+          <Button className="w-full rounded-md py-6">
+            <span className="uppercase tracking-wider font-semibold">
+              ADD TO CART
+            </span>
+          </Button>
+        </div>
       </div>
       <h3 className="text-medium font-serif pt-2 text-sm text-gray-500">
         {item.brand}
